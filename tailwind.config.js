17d7958d4d123244046c3e2916d08f8f97752092
NdharuW1402 <ndharuwijayanto14@gmail.com>
2024-02-21 14:30:06 +0700

    penambahan hero section dan progres pengerjaan

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ['index.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        prymary:'#14b8a6',
+        dark:'#0f172a',
+      },
+    },
   },
   plugins: [],
 }
